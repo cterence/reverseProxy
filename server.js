86ac7b7ecbca7f2615b3e5503f7e1fd613176be0
Térence Chateigné <terence.chateigne@gmail.com>
2020-04-27 21:12:27 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const fs = require("fs");
+const http = require("http");
+const https = require("https");
+
+const { routes } = require("./config.json");
+const certificate = fs.readFileSync(process.env.FULLCHAIN, "utf-8");
+const privateKey = fs.readFileSync(process.env.PRIVKEY, "utf-8");
+const credentials = { key: privateKey, cert: certificate };
+
+const app = express();
+
+for (route of routes) {
+  app.use(
+    route.route,
+    createProxyMiddleware({
+      target: route.address,
+      pathRewrite: (path, req) => {
+        return path.split("/").slice(2).join("/");
+      },
+    })
+  );
+}
+
+app.get("/", (req, res) => {
+  res.redirect(req.headers.referer);
+});
+
+const httpsServer = https.createServer(credentials, app);
+httpsServer.listen(443);
