b28e3949ba9760a6c20a4037cfc4dcd0a9b342f0
Térence Chateigné <terence.chateigne@gmail.com>
2020-04-29 00:48:57 +0200

    ejs

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const { createProxyMiddleware } = require('http-proxy-middleware')
-const fs = require('fs')
-const https = require('https')
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const fs = require("fs");
+const http = require("http");
+const https = require("https");
 
-const { routes } = require('./config.json')
-const certificate = fs.readFileSync(process.env.FULLCHAIN, 'utf-8')
-const privateKey = fs.readFileSync(process.env.PRIVKEY, 'utf-8')
-const credentials = { key: privateKey, cert: certificate }
+const { routes } = require("./config.json");
+const certificate = fs.readFileSync(process.env.FULLCHAIN, "utf-8");
+const privateKey = fs.readFileSync(process.env.PRIVKEY, "utf-8");
+const credentials = { key: privateKey, cert: certificate };
 
-const app = express()
+const app = express();
+
+app.use((req, res, next) => {
+    if (req.secure) next();
+    else res.redirect("https://" + req.headers.host + req.url);
+});
+
+app.set("view engine", "ejs");
 
 for (route of routes) {
     app.use(
@@ -18,16 +26,21 @@ for (route of routes) {
         createProxyMiddleware({
             target: route.address,
             pathRewrite: (path, req) => {
-                return path.split('/').slice(2).join('/')
+                return path.split("/").slice(2).join("/");
             },
+            logLevel: "debug",
         })
-    )
+    );
 }
 
-app.get('/', (req, res) => {
-    if (req.headers.referer) res.redirect(req.headers.referer)
-    else res.sendStatus(404)
-})
+app.get("/", (req, res) => {
+    if (req.headers.referer) res.redirect(req.headers.referer);
+    else
+        res.render("index", {
+            routes,
+            url: req.protocol + "://" + req.hostname,
+        });
+});
 
-const httpsServer = https.createServer(credentials, app)
-httpsServer.listen(443)
+http.createServer(app).listen(80);
+https.createServer(credentials, app).listen(443);
