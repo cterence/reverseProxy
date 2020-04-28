484d2803a183599a543fbc7f672e090eef906860
Térence Chateigné <terence.chateigne@gmail.com>
2020-04-28 23:34:17 +0200

    prettier

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
-require("dotenv").config();
+require('dotenv').config()
 
-const express = require("express");
-const { createProxyMiddleware } = require("http-proxy-middleware");
-const fs = require("fs");
-const http = require("http");
-const https = require("https");
+const express = require('express')
+const { createProxyMiddleware } = require('http-proxy-middleware')
+const fs = require('fs')
+const https = require('https')
 
-const { routes } = require("./config.json");
-const certificate = fs.readFileSync(process.env.FULLCHAIN, "utf-8");
-const privateKey = fs.readFileSync(process.env.PRIVKEY, "utf-8");
-const credentials = { key: privateKey, cert: certificate };
+const { routes } = require('./config.json')
+const certificate = fs.readFileSync(process.env.FULLCHAIN, 'utf-8')
+const privateKey = fs.readFileSync(process.env.PRIVKEY, 'utf-8')
+const credentials = { key: privateKey, cert: certificate }
 
-const app = express();
+const app = express()
 
 for (route of routes) {
-  app.use(
-    route.route,
-    createProxyMiddleware({
-      target: route.address,
-      pathRewrite: (path, req) => {
-        return path.split("/").slice(2).join("/");
-      },
-    })
-  );
+    app.use(
+        route.route,
+        createProxyMiddleware({
+            target: route.address,
+            pathRewrite: (path, req) => {
+                return path.split('/').slice(2).join('/')
+            },
+        })
+    )
 }
 
-app.get("/", (req, res) => {
-  res.redirect(req.headers.referer);
-});
+app.get('/', (req, res) => {
+    if (req.headers.referer) res.redirect(req.headers.referer)
+    else res.sendStatus(404)
+})
 
-const httpsServer = https.createServer(credentials, app);
-httpsServer.listen(443);
+const httpsServer = https.createServer(credentials, app)
+httpsServer.listen(443)
